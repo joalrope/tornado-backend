4e4c63f3044dc936ff214764f39defceb8efabb6
José Rodríguez <joalrope@gmail.com>
2022-12-08 12:31:30 -0400

    node?module / dist folder exclude

--- dist/routes/cron.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cronRouter = void 0;
-const express_1 = require("express");
-const cron_1 = require("../controllers/cron");
-exports.cronRouter = (0, express_1.Router)();
-exports.cronRouter.post('/cron', [], cron_1.cronController);
